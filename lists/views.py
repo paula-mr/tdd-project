3b6b4d016b54f0d9dd47bdd94cde87357f9b7c69
Paula Ribeiro <paula.mribeiro05@gmail.com>
2022-01-29 20:28:34 -0300

    Implementando o Novo Projeto: View para Adicionar Items em uma Lista Existente

--- lists/views.py
+++ lists/views.py
@@ -8,8 +8,13 @@ def view_list(request, list_id):
     my_list = List.objects.get(id=list_id)
     items = Item.objects.filter(list=my_list)
     return render(request, 'list.html', {'items': items})
-    
+
 def new_list(request):
     my_list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=my_list)
     return redirect(f'/lists/{my_list.id}/')
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
